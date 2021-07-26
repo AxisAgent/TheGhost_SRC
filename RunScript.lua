8e5c8d4f3551a16ea358115c78d7efd0fa63c0dc
AxisAgent <77366347+AxisAgent@users.noreply.github.com>
2021-07-26 14:52:55 -0700

    a run and stamina script

---
+++ RunScript.lua
@@ -0,0 +1,37 @@
+local plr = game.Players.LocalPlayer
+local char = plr.Character
+local hum = char:WaitForChild('Humanoid')
+local uis = game:GetService("UserInputService")
+local stamina = Instance.new("IntValue")
+stamina.Value = 100
+local running = false
+
+uis.InputBegan:Connect(function(ip)
+	if ip.KeyCode == Enum.KeyCode.LeftShift then
+		if stamina.Value > 1 then
+			running = true
+		end
+		hum.WalkSpeed = 21
+	end
+end)
+uis.InputEnded:Connect(function(ip)
+	if ip.KeyCode == Enum.KeyCode.LeftShift then
+		running = false
+	    hum.WalkSpeed = 16
+	end
+end)
+
+while true do
+	wait(0.0001)
+	if running == true then
+		wait(0.1)
+		stamina.Value = stamina.Value - 1
+		local xScale = stamina.Value/20
+		local xOffset = 0
+		script.Parent.ImageLabel.TweenFrame:TweenSize(UDim2.new(xScale,xOffset,0, 38),Enum.EasingDirection.In,Enum.EasingStyle.Quad,0.1)
+		print(stamina.Value)
+    	if stamina.Value < 1 then
+			running = false
+		end
+	end
+end
