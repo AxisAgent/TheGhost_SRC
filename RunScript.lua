66e7d5e73da9e10a22956c2678a1693e968a8fbf
AxisAgent <77366347+AxisAgent@users.noreply.github.com>
2021-08-05 16:18:09 -0700

    the old one broke so im making this for temp

--- RunScript.lua
+++ RunScript.lua
@@ -3,41 +3,15 @@ local char = plr.Character
 local hum = char:WaitForChild('Humanoid')
 local loaded = hum:LoadAnimation(script.Parent.RUN)
 local uis = game:GetService("UserInputService")
-local stamina = Instance.new("IntValue")
-stamina.Value = 100
-local running = false
-
 uis.InputBegan:Connect(function(ip)
 	if ip.KeyCode == Enum.KeyCode.LeftShift then
-		if stamina.Value > 1 then
-		    script.Breath:Stop()
-	            loaded:Play()
-		    running = true
 		hum.WalkSpeed = 21
-		end
+		loaded:Play()
 	end
 end)
 uis.InputEnded:Connect(function(ip)
 	if ip.KeyCode == Enum.KeyCode.LeftShift then
 		loaded:Stop()
-		running = false
-	    hum.WalkSpeed = 16
+		hum.WalkSpeed = 10
 	end
 end)
-
-while true do
-	wait(0.0001)
-	if running == true then
-		wait(0.1)
-		stamina.Value = stamina.Value - 1
-		local xScale = stamina.Value/20
-		local xOffset = 0
-		script.Parent.ImageLabel.TweenFrame:TweenSize(UDim2.new(xScale,xOffset,0, 38),Enum.EasingDirection.In,Enum.EasingStyle.Quad,0.1)
-		print(stamina.Value)
-    	if stamina.Value < 1 then
-			hum.WalkSpeed = 13
-			script.Breath:Play()
-			running = false
-		end
-	end
-end
